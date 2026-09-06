a9050f2a03c7e2d5c86818a885710d6292030d08
Laura V Cardona <lcvalentina3212401957@gmail.com>
2026-09-06 16:50:09 -0500

    Cambios en el navbar y el header

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -5,9 +5,8 @@ function Header() {
 
   return (
     <header>
-      <img className="logotipo" src="src/imagenes/Zyntra-Logotipo1.png" alt="Logotipo" />
+      <img className="logotipo" src="src/imagenes/logo.webp" alt="Logotipo" />
       <p>Empresa de tecnologia enfocada en el entretenimiento, diseñamos software relacionado con la cultura pop conectando con la gente a través del arte, el entrtetenimiento y la cultura geek.</p>
-      <img className="icono" src="src/imagenes/Zyntra-icono.svg" alt="Logotipo" />
     </header>
   )
 }
